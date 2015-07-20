20fd5c2f825e3d10cfba39e5e9270e5cda3b5950
berkcaputcu <berk.caputcu@shopify.com>
2015-07-20 17:10:59 -0400

    Initial Commit

---
+++ Jukapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jukapp
+//
+//  Created by Berk Caputcu on 2015-07-20.
+//  Copyright (c) 2015 Berk Caputcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
